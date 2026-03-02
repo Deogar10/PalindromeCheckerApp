bdbb35292d58c6f66c6f8a918e4b18a2558af089
NULL <NULL>
2026-03-02 09:04:33 +0530

    UC1: Application Entry & Welcome Message

---
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -0,0 +1,11 @@
+/*
+ *@author DEO
+ * @Version 1.0
+ */
+public class PalindromeCheckerApp {
+    public static  void main(String[] args){
+        System.out.println("PalindromeCheckerApp");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully.");
+    }
+}
